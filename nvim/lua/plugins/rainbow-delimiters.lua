05507f60d019a0ead9f0748fbf4027b1135e7b64
James Noria <contact@jamesnoria.com>
2026-07-18 23:02:21 -0500

    brackets colors

---
+++ nvim/lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      vim.g.rainbow_delimiters = {
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterCyan",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+        },
+      }
+
+      local function set_rainbow_highlights()
+        vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#ff6b6b" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#feca57" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#48dbfb" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#1dd1a1" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#ff9ff3" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#54a0ff" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#ff9f43" })
+      end
+
+      set_rainbow_highlights()
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = set_rainbow_highlights,
+        desc = "Apply custom rainbow delimiter colors",
+      })
+    end,
+  },
+}
