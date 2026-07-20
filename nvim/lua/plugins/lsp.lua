161808177b7611b5f07ba549a3e2f8cdc156a932
James Noria <contact@jamesnoria.com>
2026-07-19 20:37:49 -0500

    fix for js files

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,10 +31,37 @@ return {
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      local function get_global_tsserver_path()
+        local npm_root = vim.fn.systemlist("npm root -g")[1]
+
+        if vim.v.shell_error ~= 0 or not npm_root or npm_root == "" then
+          return nil
+        end
+
+        local tsserver = npm_root .. "/typescript/lib/tsserver.js"
+        if vim.fn.filereadable(tsserver) == 1 then
+          return tsserver
+        end
+
+        return nil
+      end
+
       for _, server in ipairs(opts.ensure_installed) do
-        vim.lsp.config(server, {
+        local config = {
           capabilities = capabilities,
-        })
+        }
+
+        if server == "ts_ls" then
+          local tsserver_path = get_global_tsserver_path()
+
+          config.init_options = {
+            tsserver = {
+              fallbackPath = tsserver_path,
+            },
+          }
+        end
+
+        vim.lsp.config(server, config)
         vim.lsp.enable(server)
       end
     end,
